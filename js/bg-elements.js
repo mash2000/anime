43b210aab300f1f27329355e3a7c578d675087ca
kamshir <iammaksim2000shirshikov@gmail.com>
2023-07-27 11:58:24 +0300

    modalInput

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -3,5 +3,4 @@ const elements = document.querySelectorAll('.set-bg')
 elements.forEach(elem => {
   const src = elem.dataset.setbg
   elem.style.backgroundImage = `url(${src})`
-  console.log(src);
 })
--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector('.search-model')
 const modalBtn = document.querySelector('.icon_search')
 const modalClose = document.querySelector('.search-close-switch')
+const modalInput = document.querySelector('#search-input')
 
 modalBtn.addEventListener('click', () => {
   modal.style.display = 'block'
@@ -8,4 +9,8 @@ modalBtn.addEventListener('click', () => {
 
 modalClose.addEventListener('click', () => {
   modal.style.display = 'none'
+})
+
+modalInput.addEventListener('input', (e) => {
+  console.log(e.currentTarget.value);
 })
