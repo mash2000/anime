5c1c85481f3dbf3ad7f18bfcd92bc7112a7333df
kamshir <iammaksim2000shirshikov@gmail.com>
2023-07-27 11:49:15 +0300

    modal

---
+++ js/bg-elements.js
@@ -0,0 +1,7 @@
+const elements = document.querySelectorAll('.set-bg')
+
+elements.forEach(elem => {
+  const src = elem.dataset.setbg
+  elem.style.backgroundImage = `url(${src})`
+  console.log(src);
+})
