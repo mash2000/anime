439ef6ae6a96fe79ab9a440873c1ecf3bc3553c3
kamshir <iammaksim2000shirshikov@gmail.com>
2023-07-30 13:07:58 +0300

    delete Views All

--- js/categoriesData.js
+++ js/categoriesData.js
@@ -29,11 +29,6 @@ const categoriesData = () => {
                             <h4>${ganre}</h4>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-4 col-sm-4">
-                        <div class="btn__all">
-                            <a href="/categories.html?ganre=${ganre}" class="primary-btn">View All <span class="arrow_right"></span></a>
-                        </div>
-                    </div>
                 </div>
             `)
 
